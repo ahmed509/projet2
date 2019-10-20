dd7137f1211184b79580123a52ea4f70c8326287
ahmed509 <53703321+ahmed509@users.noreply.github.com>
2019-10-20 18:19:04 +0200

    Add files via upload

---
+++ p2/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return (
+    <div style={{ maxWidth:"100vw", border:"solde 1px red"}}>
+
+    <h1 className="title red">My title</h1>
+  
+    <br/>
+  
+  <img src="logo192.png" />
+  
+  <br/>
+  
+    <img src="2.jpg" />
+  
+  
+  </div>
+  );
+}
+
+export default App;
